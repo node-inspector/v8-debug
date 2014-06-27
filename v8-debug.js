1e8aa84573d7299923f0e58714f3082ea1baf8da
3y3 <3y3@bk.ru>
2014-06-27 17:44:05 +0400

    Safely handling errors

--- v8-debug.js
+++ v8-debug.js
@@ -18,11 +18,54 @@ binding.call(function WRAP_debugCommandProcessor(exec_state) {
   };
 
   processor.extendedProcessDebugJSONRequest_ = function(json_request) {
-    var request = JSON.parse(json_request);
-    if (typeof this.extendedProcessDebugJSONRequestHandles_[request.command] == 'function') {
-      var response = this.createResponse(request)
-      this.extendedProcessDebugJSONRequestHandles_[request.command].call(this, request, response);
-      return response.toJSONProtocol();
+    var request;  // Current request.
+    var response;  // Generated response.
+    try {
+      try {
+        // Convert the JSON string to an object.
+        request = JSON.parse(json_request);
+        
+        if (typeof this.extendedProcessDebugJSONRequestHandles_[request.command] !== 'function') return;
+        
+        // Create an initial response.
+        response = this.createResponse(request);
+        
+        if (request.arguments) {
+          var args = request.arguments;
+          if (args.maxStringLength !== undefined) {
+            response.setOption('maxStringLength', args.maxStringLength);
+          }
+        }
+        
+        this.extendedProcessDebugJSONRequestHandles_[request.command].call(this, request, response);
+      } catch (e) {
+        // If there is no response object created one (without command).
+        if (!response) {
+          response = this.createResponse();
+        }
+        response.success = false;
+        response.message = e.toString();
+      }
+
+      // Return the response as a JSON encoded string.
+      try {
+        if (response.running !== undefined) {
+          // Response controls running state.
+          this.running_ = response.running;
+        }
+        response.running = this.running_;
+        return response.toJSONProtocol();
+      } catch (e) {
+        // Failed to generate response - return generic error.
+        return '{"seq":' + response.seq + ',' +
+                '"request_seq":' + request.seq + ',' +
+                '"type":"response",' +
+                '"success":false,' +
+                '"message":"Internal error: ' + e.toString() + '"}';
+      }
+    } catch (e) {
+      // Failed in one of the catch blocks above - most generic error.
+      return '{"seq":0,"type":"response","success":false,"message":"Internal error"}';
     }
   };
   
