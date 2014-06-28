278fcbcc510632d1d321ec1b4344808bb6d92ba6
3y3 <3y3@bk.ru>
2014-06-29 00:59:51 +0400

    Remove 'v8-debug' from cache after disconnect

--- v8-debug.js
+++ v8-debug.js
@@ -4,15 +4,18 @@ var EventEmitter = require('events').EventEmitter;
 binding.call(function WRAP_debugCommandProcessor(exec_state) {
   var processor = exec_state.debugCommandProcessor(true).__proto__;
   var oldProcessDebugRequest = processor.processDebugRequest;
-  
+
   processor.extendedProcessDebugJSONRequestHandles_ = {
     'disconnect': function(request, response) {
+      var Module = require('module').Module;
+      var moduleKey = module.id.replace(/\\\\|\/\//g, '$1');
+
       processor.processDebugRequest = oldProcessDebugRequest;
       delete processor.extendedProcessDebugJSONRequest_;
       delete processor.extendedProcessDebugJSONRequestHandles_;
-      
+      delete Module._cache[moduleKey];
+
       processor.disconnectRequest_(request, response);
-      
       module.exports.emit('close');
     }
   };
@@ -24,19 +27,19 @@ binding.call(function WRAP_debugCommandProcessor(exec_state) {
       try {
         // Convert the JSON string to an object.
         request = JSON.parse(json_request);
-        
+
         if (typeof this.extendedProcessDebugJSONRequestHandles_[request.command] !== 'function') return;
-        
+
         // Create an initial response.
         response = this.createResponse(request);
-        
+
         if (request.arguments) {
           var args = request.arguments;
           if (args.maxStringLength !== undefined) {
             response.setOption('maxStringLength', args.maxStringLength);
           }
         }
-        
+
         this.extendedProcessDebugJSONRequestHandles_[request.command].call(this, request, response);
       } catch (e) {
         // If there is no response object created one (without command).
@@ -68,7 +71,7 @@ binding.call(function WRAP_debugCommandProcessor(exec_state) {
       return '{"seq":0,"type":"response","success":false,"message":"Internal error"}';
     }
   };
-  
+
   processor.processDebugRequest = function WRAPPED_BY_NODE_INSPECTOR(request) {
     return this.extendedProcessDebugJSONRequest_(request)
       || this.processDebugJSONRequest(request);
