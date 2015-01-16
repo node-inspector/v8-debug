082ee81216995e77a620a1ac5df7ba750e15af71
3y3 <3y3@bk.ru>
2015-01-16 18:16:12 +0300

    Node-pre-gyp updated to ^0.6.0

--- test/binding.js
+++ test/binding.js
@@ -1,9 +1,12 @@
 var expect = require('chai').expect;
+var binary = require('node-pre-gyp');
+var path = require('path');
+var binding_path = binary.find(path.resolve(path.join(__dirname,'../package.json')));
 
-describe('Binary source', function() {
+describe('binding', function() {
   var binding;
-  it('was builded and can be accessed from script', function() {
-    binding = require('../build/Release/debug');
+  it('source was builded and can be accessed from script', function() {
+    binding = require(binding_path);
     expect(binding).to.be.instanceof(Object);
   });
 });
